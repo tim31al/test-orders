1899bb89d17e37bcc0e25ef305f6261ef10f4828
Alexandr Timofeev <tim31al@gmail.com>
2022-05-01 13:40:13 +0300

    init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ docker-compose up -d
 
 sleep 5
 
-docker-compose exec -u app app php composer install
-docker-compose exec -u app app php bin/console doctrine:migrations:migrate
-docker-compose exec -u app app php bin/console doctrine:fixrures:load -q
+docker-compose exec -u app app composer update
+docker-compose exec -u app app php bin/console doctrine:migrations:migrate -q
+docker-compose exec -u app app php bin/console doctrine:fixtures:load -q
